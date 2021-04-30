f83037314251b7f88c761201591d4dbc562f5a80
하수용 <syha@megazone.com>
2021-04-30 17:34:47 +0900

    build test

--- script/service_validate.sh
+++ script/service_validate.sh
@@ -2,7 +2,7 @@
 
 while [ true ]
 do
-    if [ "$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost")" = '200' ]
+    if [ "$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:8080")" = '200' ]
     then
         exit 0
     else
