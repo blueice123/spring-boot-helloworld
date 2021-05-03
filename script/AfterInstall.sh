9c2feefde4731d300d0b2f9b9be50862e0593a56
하수용 <syha@megazone.com>
2021-05-03 18:17:06 +0900

    test

--- script/AfterInstall.sh
+++ script/AfterInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TM=$(date +%F_%T)
 # CodeDeploy로 배포된 파일의 권한 변경 
-sudo chown -R ec2-user.  /spring-boot-helloworld/release
+sudo chown -R ec2-user. /spring-boot-helloworld/release
 
 # # 이전에 실행되었던 jar 파일을 백업 
 # mv /spring-boot-helloworld/src-0.0.1-SNAPSHOT.jar /spring-boot-helloworld/$TM.jar
--- script/BeforInstall.sh
+++ script/BeforInstall.sh
@@ -2,6 +2,6 @@
 
 # 기존의 relese 환경을 삭제
 if [ -d /spring-boot-helloworld/release ]; then
-    rm -rf /spring-boot-helloworld/release
+    sudo rm -rf /spring-boot-helloworld/release
 fi
-mkdir -vp /spring-boot-helloworld/release
+sudo mkdir -vp /spring-boot-helloworld/release
