b9f442917f3944fbd09073431863767765c4a54b
하수용 <syha@megazone.com>
2021-05-03 18:34:36 +0900

    test

--- script/ApplicationStart.sh
+++ script/ApplicationStart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd /spring-boot-hellowor/
+cd /spring-boot-helloworld/
 sudo nohup java -jar -Dserver.port=8080 \
         ./src-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
 sleep 10
