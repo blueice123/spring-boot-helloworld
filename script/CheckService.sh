1aecd5cfa90326a5f75d6afe4b212224c5a9b830
하수용 <syha@megazone.com>
2021-05-04 13:17:17 +0900

    test

--- script/CheckService.sh
+++ script/CheckService.sh
@@ -1,19 +1,24 @@
 #!/bin/bash 
-case "$1" in
-        start)
-            sh /spring-boot-helloworld/ApplicationStart.sh
-            ;;
-        stop)
-            sh /spring-boot-helloworld/ApplicationStop.sh
-            ;;
-        status)
-            netstat -nlp | grep 8080
-            ;;
-        restart)
-            stop
-            start
-            ;;
-        *)
-            echo $"Usage: $0 {start|stop|restart|status}"
-            exit 1
+
+case $1 in
+
+  start)
+    echo -n "start"
+    sh /spring-boot-helloworld/release/ApplicationStart.sh
+    ;;
+
+  stop)
+    echo -n "stop"
+    sh /spring-boot-helloworld/release/ApplicationStop.sh
+    ;;
+
+  status)
+    echo -n "status"
+    sh /spring-boot-helloworld/release/ValidateService.sh
+    ;;
+
+  *)
+    echo -n $"Usage: $0 {start|stop|restart|status}"
+    exit 1
+    ;;
 esac
