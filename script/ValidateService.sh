49f5a8aea623107be40ee683b09126c53fdde38a
하수용 <syha@megazone.com>
2021-05-24 10:01:21 +0900

    test

--- script/ValidateService.sh
+++ script/ValidateService.sh
@@ -4,6 +4,7 @@ while [ true ]
 do
     if [ "$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:8080")" = '200' ]
     then
+        echo "systemctl restart codedeploy-agent" | at -M now + 2 minute;
         exit 0
     else
         echo "check server is running?"
